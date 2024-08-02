0c0b8f0dcbf90f7352491f24781304c643f67018
Jimmy <ultish@gmail.com>
2024-08-02 21:57:50 +1000

    clean

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,6 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
-  // @service session;
   @service auth;
-
-  // @action
-  // invalidateSession() {
-  //   this.session.invalidate();
-  // }
 }
