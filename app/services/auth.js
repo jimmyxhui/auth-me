904ed6c0c94f9f87fbf76410e67f01a4decd4f79
Jimmy <ultish@gmail.com>
2024-08-03 15:05:15 +1000

    authenticated

--- app/services/auth.js
+++ app/services/auth.js
@@ -8,6 +8,8 @@ export default class AuthService extends Service {
   @tracked
   currentUser;
   userManager;
+  @tracked
+  roles;
 
   constructor() {
     super(...arguments);
@@ -17,24 +19,47 @@ export default class AuthService extends Service {
       client_id: 'root',
       redirect_uri: `${window.location.origin}/callback`,
       response_type: 'code',
-      scope: 'openid',
+      scope: 'openid roles',
       post_logout_redirect_uri: `${window.location.origin}/logout-callback`,
       userStore: new WebStorageStateStore({ store: window.localStorage }),
+      loadUserInfo: true, // Ensure user info is loaded
     });
 
     this.userManager.events.addUserLoaded((user) => {
       this.currentUser = user;
+
+      this.roles = this.getRoles(user);
     });
 
     this.userManager.events.addUserUnloaded(() => {
       this.currentUser = null;
+      this.roles = null;
     });
 
     this.userManager.getUser().then((user) => {
       if (user) {
         this.currentUser = user;
+        this.roles = this.getRoles(user);
       }
     });
+  }
+
+  getRoles(user) {
+    // Assuming roles are included in the ID token or access token
+    const idToken = user?.id_token;
+    const accessToken = user?.access_token;
+
+    if (idToken) {
+      const idTokenPayload = JSON.parse(atob(idToken.split('.')[1]));
+      return idTokenPayload.roles || [];
+    }
+
+    if (accessToken) {
+      const accessTokenPayload = JSON.parse(atob(accessToken.split('.')[1]));
+      return accessTokenPayload.roles || [];
+    }
+
+    return [];
   }
 
   @action
@@ -55,5 +80,9 @@ export default class AuthService extends Service {
   @action
   async handleLogoutCallback() {
     await this.userManager.signoutRedirectCallback();
+  }
+
+  get accessToken() {
+    return this.currentUser?.access_token;
   }
 }
